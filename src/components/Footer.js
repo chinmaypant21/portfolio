3d85ee7f33ed68cc937369f54cc74833df677cf3
chinmaypant21 <pant.chinu.2@gmail.com>
2023-03-29 03:43:41 +0530

    Logo in small media

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,9 @@
-// import { Container, Row, Col } from "react-bootstrap";
-import logo from "../assets/img/logo.svg";
-import navIcon1 from "../assets/img/nav-icon1.svg";
-import navIcon2 from "../assets/img/nav-icon2.svg";
-import navIcon3 from "../assets/img/nav-icon3.svg";
+import navIconLinkedinn from "../assets/img/nav-icon1.svg";
+import navIconInsta from "../assets/img/nav-icon3.svg";
 import navIconTwitter from "../assets/img/nav-icon-twitter.svg"
 import navIconGithub from "../assets/img/nav-icon-github.svg"
 import navIconWebsite from "../assets/img/nav-icon-website.svg"
-
-import { useState } from "react";
+import logo from '../assets/img/logo.png';
 import { Widget } from '@typeform/embed-react'
 import { Container, Row, Col } from "react-bootstrap";
 import contactImg from "../assets/img/contact-img.svg";
@@ -45,11 +41,14 @@ export const Footer = () => {
           <Row className="align-items-center">
             <Col size={12} sm={6} className="text-center" style={{width:'100%'}}>
               <div className="social-icon">
-                <a href="https://www.linkedin.com/in/chinmay-pant-565277200/" target="_blank"><img src={navIcon1} alt="Icon" /></a>
+                <a href="https://www.linkedin.com/in/chinmay-pant-565277200/" target="_blank"><img src={navIconLinkedinn} alt="Icon" /></a>
                 <a href="https://github.com/chinmaypant21" target="_blank"><img src={navIconGithub} alt="Icon" /></a>
                 <a href="https://twitter.com/not__cicada" target="_blank"><img src={navIconTwitter} alt="Icon" /></a>
                 <a href="https://chinz.me" target="_blank"><img src={navIconWebsite} alt="Icon" /></a>
-                <a href="https://instagram.com/not_cicada"><img src={navIcon3} alt="Icon" /></a>
+                <a href="https://instagram.com/not_cicada" target="_blank"><img src={navIconInsta} alt="Icon" /></a>
+              </div>
+              <div className="d-md-none">
+                <img src={logo} />
               </div>
             </Col>
           </Row>
